d538bc9f824d953e9fca7049d92f12aca6022d60
wangjunlin <wangjunlin.thu@outlook.com>
2025-12-11 10:42:37 +0800

    删除多余文件

--- temp.py
+++ temp.py
@@ -1,27 +1,37 @@
-import pyautogui
 from utils import *
-from actions import *
+from game_states import *
 
+def get_location():
+    curx, cury = pyautogui.position()
+    print(str(curx) + ', ' + str(cury))
 
-# get_location()
+def save_image(image_path, lx, ly, rx, ry):
+    click_top()
+    width, height = rx - lx, ry - ly
+    region = (lx, ly, width, height)
+    pyautogui.screenshot(image_path, region=region)
 
-click_top()
-save_image(lx=1594, ly=876, rx=1832, ry=978, image_path='img/combat/precombat.png')
+def getbox(image_path, lx, ly, rx, ry):
+    with open('image_region.json', 'r') as f:
+        data = json.load(f)
+    data[image_path] = [lx, ly, rx-lx, ry-ly]
+    with open('image_region.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
-# print(is_image(image_path='img/glass_window.png'))
+# click_top()
+# print(comb_2.condition())
+# pyautogui.locateOnScreen('img/combat/give_command.png', confidence=0.9)
+get_location()
 
-# clear_energy()
+# getbox('img/combat/acquire_ego_accessory.png',
+#        247, 212,
+#         1736, 370
+#        )
 
-# drag(820, 235, 1123, 473)
+# save_image(lx=1373, ly=154, rx=1833, ry=289, image_path='img/outside/glass_window.png')
 
-# play_one_layer(1)
-# combat_react()
+# pyautogui.screenshot('aaa.png', region=(1373,
+#         154,
+#         510,
+#         135))
 
-# print(is_interface('img/combat/give_command.png'))
-
-# pyautogui.click(900, 510)
-
-# pyautogui.screenshot('img/combat/precombat.png', region=(1581,
-#         875,
-#         254,
-#         101))
